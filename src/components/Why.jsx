0422669b51e82e585073b755f891895da9965515
AshwiniPerewar@gmail.com <ashwiniperewar411@gmail.com>
2023-02-03 17:35:28 +0530

    completed

--- src/components/Why.jsx
+++ src/components/Why.jsx
@@ -13,28 +13,30 @@ const Why = () => {
         <p class="text-2xl font-bold text-white">
           Take on any Challenge of the Digital World
         </p>
-        
+
         <div class=" flex  space-x-2 text-stone-300 text-xs">
-        <i class="fa-solid fa-circle-check mt-1"></i>
-        <p>Corporate Financial Advisory</p>
+          <i class="fa-solid fa-circle-check mt-1"></i>
+          <p>Corporate Financial Advisory</p>
         </div>
         <div class=" flex  space-x-2 text-stone-300 text-xs">
-        <i class="fa-solid fa-circle-check mt-1"></i>
-        <p>Development of Financial Models</p>
+          <i class="fa-solid fa-circle-check mt-1"></i>
+          <p>Development of Financial Models</p>
         </div>
         <div class=" flex  space-x-2 text-stone-300 text-xs">
-        <i class="fa-solid fa-circle-check mt-1"></i>
-        <p>Deal Structuring</p>
+          <i class="fa-solid fa-circle-check mt-1"></i>
+          <p>Deal Structuring</p>
         </div>
         <div class=" flex  space-x-2">
-           <div class="border-b-2">
-           <input class="bg-inherit text-md text-white mt-4 font-bold" placeholder="Enter your email id"></input>
-           </div> 
-        <button class="bg-red-500 rounded-2xl pl-2 mt-4 h-8  text-white w-32 text-xs ">
-          Get Started
-        </button>
+          <div class="border-b-2">
+            <input
+              class="bg-inherit text-md text-white mt-4 font-bold"
+              placeholder="Enter your email id"
+            ></input>
+          </div>
+          <button class="bg-red-500 rounded-2xl pl-2 mt-4 h-8  text-white w-32 text-xs ">
+            Get Started
+          </button>
         </div>
-        
       </div>
     </div>
   );
